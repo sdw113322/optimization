27fdfd52dcffdbbb3e2b32b3f539c791df565e19
sdw113322 <sdw113322@gmail.com>
2015-05-17 22:32:48 +0800

    2

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -53,6 +53,22 @@ QString QuasiOut(vector<array<double,6> > input){
     return result;
 }
 
+QString ConOut(vector<array<double,7> > input){
+    QString result;
+    result += "<table><thead><tr><th>i</th><th>x</th><th>y</th><th>x of gradient f</th><th>y of gradient f</th><th>beta</th><th>f(x,y)</th></tr></thead><tbody>";
+    for(int i=0;i<input.size();i++){
+        result += "<tr>";
+        for(int j=0;j<7;j++){
+            result += "<td>";
+            result += QString::number(input[i][j]);
+            result += "</td>";
+        }
+        result += "</tr>";
+    }
+    result += "</tbody></table>";
+    return result;
+}
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -117,5 +133,24 @@ void MainWindow::on_pushButton_4_clicked()
     string str = input.toStdString();
     Polys a(str);
     QString o = QuasiOut(quasi(a,ix,iy,x_max,x_min,y_max,y_min,0.000001));
+    ui->textBrowser->append(o);
+}
+
+void MainWindow::on_pushButton_6_clicked()
+{
+    QString input = ui->lineEdit->text();
+    QString x = ui->lineEdit_2->text();
+    QString y = ui->lineEdit_3->text();
+    QString initial = ui->lineEdit_4->text();
+    std::string::size_type sz;
+    double x_min = std::stod (x.toStdString().substr(1),&sz);
+    double x_max = std::stod (x.toStdString().substr(sz+2));
+    double y_min = std::stod (y.toStdString().substr(1),&sz);
+    double y_max = std::stod (y.toStdString().substr(sz+2));
+    double ix = std::stod (initial.toStdString().substr(1),&sz);
+    double iy = std::stod (initial.toStdString().substr(sz+2));
+    string str = input.toStdString();
+    Polys a(str);
+    QString o = ConOut(conjugate(a,ix,iy,x_max,x_min,y_max,y_min,0.000001));
     ui->textBrowser->append(o);
 }
